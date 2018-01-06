66f6bd8b1810c7420863b9c2071f6c787607acbd
abhig1997 <gupta257@purdue.edu>
2018-01-06 00:38:26 -0800

    finished all the code to view averages

--- nbacli.py
+++ nbacli.py
@@ -7,6 +7,8 @@ from nba_py import game
 import boxscore_functions
 import player_functions
 
+printer = pprint.PrettyPrinter(indent=4) # my PrettyPrinter
+
 # def get_player():
 #     response = player.get_player(first_name='Kyrie', last_name='Irving', season='2016-17', only_current=0, just_id=False)
 #     # print(type(response))
@@ -17,20 +19,17 @@ import player_functions
 #     dataframe = game.BoxscoreSummary("0021700570")
 #     print(dataframe.line_score())
 
-"""
-Prints the main menu for the program
-"""
-def print_main_menu():
-    print('What would you like to do?')
-    print('1. Access boxscore functions')
-    print('9. Exit the program')
+
+def get_career_totals(id):
+    player_career = player.PlayerCareer(id)
+    printer.pprint(player_career.regular_season_career_totals())
 
 
 """
 To run the program
 """
 def main():
-    printer = pprint.PrettyPrinter(indent=4)
+
 
     print('Welcome to Abhi\'s NBA CLI!')
     loop=True
@@ -45,8 +44,7 @@ def main():
 
             print("What information about the player would you like to view?")
             print("1. Basic Info (Vitals)")
-            print("2. View Averages For All Seasons")
-            print("3. View Regular Season Career Averages")
+            print("2. View Season Averages")
             print("9. Go back to main menu")
             choice = input("Pick a number from the list above.\n")
 
@@ -60,24 +58,41 @@ def main():
                     player_summary = player.PlayerSummary(id)
                     vitals = player_summary.info()
                     printer.pprint(vitals)
-            # getting averages for all seasons
+            # submenu for season averages
             elif int(choice) == 2:
                 id = player_functions.get_player_id(first_name, last_name)  # the id of the player requested
                 if id is None:
                     print("The player was not found")
                 else:
-                    # getting the basic player summary
+                    print("1. View Regular Season Totals")
+                    print("2. View Career Regular Season Totals")
+                    print("3. View Post Season Totals")
+                    print("4. View Career Post Season Totals")
+                    print("5. View All Star Season Totals")
+                    print("6. View Career All Star Season Totals")
+                    print("7. View College Season Totals")
+                    print("8. View Career College Season Totals")
                     player_career = player.PlayerCareer(id)
-                    printer.pprint(player_career.regular_season_totals())
-            # getting career regular season averages
-            elif int(choice) == 3:
-                id = player_functions.get_player_id(first_name, last_name)  # the id of the player requested
-                if id is None:
-                    print("The player was not found")
-                else:
-                    # getting the player career highs
-                    player_career = player.PlayerCareer(id)
-                    printer.pprint(player_career.regular_season_career_totals())
+                    choice = input("Pick a number from the list above.\n")
+                    num = int(choice)
+
+                    if num == 1:
+                        # view regular season totals
+                        printer.pprint(player_career.regular_season_totals())
+                    elif num == 2:
+                        printer.pprint(player_career.regular_season_career_totals())
+                    elif num == 3:
+                        printer.pprint(player_career.post_season_totals())
+                    elif num == 4:
+                        printer.pprint(player_career.post_season_career_totals())
+                    elif num == 5:
+                        printer.pprint(player_career.all_star_season_totals())
+                    elif num == 6:
+                        printer.pprint(player_career.career_all_star_season_totals())
+                    elif num == 7:
+                        printer.pprint(player_career.college_season_totals())
+                    elif num == 8:
+                        printer.pprint(player_career.college_season_career_totals())
             #tryna dip
             elif int(choice) == 9:
                 pass
