a497d0d3bd02e95d9dc20f9a2fb4256f37de369a
abhig1997 <gupta257@purdue.edu>
2018-01-08 09:46:12 -0500

    created constants file, finished box score url parsing

---
+++ constants.py
@@ -0,0 +1,9 @@
+teams_played = []
+
+all_scores = []
+
+final_scores = []
+
+box_score_urls = []
+
+full_urls = []
--- get_games.py
+++ get_games.py
@@ -1,43 +1,55 @@
 from bs4 import BeautifulSoup
 import requests
+from pprint import pprint
 
-teams = []
-filteredteams = []
-scores = []
-filteredscores = []
-status = []
-urls = []
-filteredurls = []
+from constants import *
 
 def find_games():
     response = requests.get("http://stats.nesn.com/nba/scoreboard.asp?meta=true")
     parsed = BeautifulSoup(response.text, 'lxml')
 
-    teams_played = [] # the list of teams that have already played today
+    # teams_played = [] # the list of teams that have already played today
 
-    all_scores = []
-    final_scores = []
+    # all_scores = []
+    # final_scores = []
+    # box_score_urls = []
+    # full_urls = []
 
-    # print(parsed)
-
+    # get all the teams that have finished games today
     for i in parsed.find_all("td", "shsLeaderTtl"):
     	# print(teams_that_played.string)
     	if "Leaders" not in i.string:
     		teams_played.append(i.string)
 
+    # get everything under the shsTotD header, these are scores from games have been played
     for j in parsed.find_all("td", "shsTotD"):
     	all_scores.append(j.string)
 
+
+   	# get final scores for games that have been played by filtering from all_scores array
     for k in range(0, len(all_scores)):
     	if all_scores[k] == 'Tot':
     		final_scores.append(all_scores[k+5])
     		final_scores.append(all_scores[k+10])
-    		# print(all_scores[k+10])
 
 
+    for i in parsed.find_all("td", "shsLiveNav"):
+    	for j in i.find_all("a", href=True):
+    		# print(j)
+    		# print(type(j["href"]))
+    		box_score_urls.append(j["href"])
+
+    for i in range(0, len(box_score_urls)):
+    	# print(i)
+    	if 'boxscore' in box_score_urls[i]:
+    		full_urls.append("http://stats.nesn.com" + box_score_urls[i])
+
+
+    pprint(full_urls)
+    # print(box_score_urls)
     # print(teams_played)
-    print(teams_played)
-    print(final_scores)
+    # print(teams_played)
+    # print(final_scores)
 
 
 
